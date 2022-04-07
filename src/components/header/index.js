b66c3cf0a5f34ad746f965ab7980588ee421f4b2
parth maheta <35091091+parthmaheta@users.noreply.github.com>
2022-04-07 10:35:25 +0530

    created header bar

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import "./header.scss"
+
+export function Header() {
+  return (
+    <div className="header flex-space-between">
+      <div className="logo">
+        <img src="./logo.svg" alt="logo img" />
+      </div>
+    </div>
+  )
+}
