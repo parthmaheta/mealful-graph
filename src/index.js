f05e8c1f14253b760837e52d39aff95ba1039e27
parth maheta <35091091+parthmaheta@users.noreply.github.com>
2022-04-08 23:02:38 +0530

    react 18 update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
 import { store } from "./store"
 
-ReactDOM.render(
+const root = document.getElementById("root")
+const container = createRoot(root)
+
+container.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
